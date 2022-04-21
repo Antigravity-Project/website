a8641bc22821e095383220435ec9be9a6a4f2192
End313234 <end313234@protonmail.com>
2022-04-21 20:04:19 -0300

    Customize scroll bar

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -9,4 +9,17 @@ export const GlobalStyle = createGlobalStyle`
         height: 100vh;
         overflow-x: hidden;
     }
+
+    ::-webkit-scrollbar {
+        width: 5px;
+    }
+
+    ::-webkit-scrollbar-thumb {
+        background-color: #6B6B6B;
+        border-radius: 2rem;
+    }
+
+    ::-webkit-scrollbar-track {
+        background-color: #16161A;
+    }
 `;
