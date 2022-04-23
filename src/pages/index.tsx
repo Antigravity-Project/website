4b35b2cccde34170c90ef15e2a87347bdf159730
End313234 <end313234@protonmail.com>
2022-04-23 20:25:29 -0300

    Change website structure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,37 +1,3 @@
-import type { NextPage } from "next";
-import Image from "next/image";
-import { BotInformation, BotInvite, ImageWrapper, Main, P } from "styles/home";
-import { Button } from "web/components/button";
-import { Header } from "web/components/header";
-
-const Home: NextPage = () => {
-	return (
-		<>
-			<Header />
-			<Main>
-				<BotInformation>
-					<BotInvite>
-						<P>
-							Antigravity é um simples bot de moderação e diversão para seu
-							servidor! Este é um projeto desenvolvido por End, Hanzo e Molotov!
-						</P>
-						<Button>Adicionar o bot</Button>
-					</BotInvite>
-					<ImageWrapper>
-						<Image
-							src="/logo.webp"
-							width="240"
-							height="220"
-							style={{
-								borderRadius: "50%",
-							}}
-							alt="logo"
-						/>
-					</ImageWrapper>
-				</BotInformation>
-			</Main>
-		</>
-	);
-};
+import { Home } from "web/pages/home";
 
 export default Home;
---
+++ src/web/pages/home/styles.ts
@@ -0,0 +1,48 @@
+import styled from "styled-components";
+import { Paragraph } from "styles";
+
+export const Main = styled.main`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	gap: 10rem;
+	height: 100vh;
+	background-color: ${({ theme }) => theme.BACKGROUND};
+`;
+
+export const BotInformation = styled.div`
+	margin-top: 5rem;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	gap: 10rem;
+
+	@media (max-width: 800px) {
+		margin-top: 2rem;
+		flex-direction: column-reverse;
+		gap: 3rem;
+	}
+`;
+
+export const P = styled(Paragraph)`
+	width: 35vw;
+	min-width: 20rem;
+	font-size: 1.2rem;
+`;
+
+export const ImageWrapper = styled.span`
+	transition: 200ms;
+
+	&:hover {
+		transform: scale(1.1);
+	}
+`;
+
+export const BotInvite = styled.div`
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	text-align: center;
+	flex-direction: column;
+	gap: 30px;
+`;
