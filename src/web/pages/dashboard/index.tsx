153887b7e89c6c42d591dcecea9b0905a83ba906
End313234 <end313234@protonmail.com>
2022-04-23 20:42:28 -0300

    Protect dashboard route

--- src/web/pages/dashboard/index.tsx
+++ src/web/pages/dashboard/index.tsx
@@ -1,9 +1,18 @@
 import type { NextPage } from "next";
+import { useEffect } from "react";
 import { Header } from "web/components/header";
 
 import { Main } from "./styles";
 
 export const Dashboard: NextPage = () => {
+	useEffect(() => {
+		const userData = localStorage.getItem("user");
+		if (!userData) {
+			window.location.href = process.env
+				.NEXT_PUBLIC_DISCORD_AUTHORIZATION_LINK as string;
+		}
+	}, []);
+
 	return (
 		<>
 			<Header />
